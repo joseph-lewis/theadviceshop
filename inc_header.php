66758b7699108bbb269689c8344256d342db42ee
Joe <jlewi142@gmail.com>
2020-04-23 11:25:39 +1000

    Header

--- inc_header.php
+++ inc_header.php
@@ -2,16 +2,10 @@
 $filename = substr(strrchr($_SERVER['SCRIPT_NAME'], "/"), 1); // missing '' around index - notice error
 $name = substr($filename, 0, strrpos($filename, ".")); ?>
 <header id="pageHeader">
-    <form method="post" action="">
+    <form method="post" action="login.php?page=<?php echo $name; ?>">
         <input type="text" name="username" id="username" placeholder="Username" value="<?= isset($_POST['something']) ? htmlspecialchars($_POST['something']) : '' ?>" />
         <input type="submit" name="submit" value="Login"/>
     </form></aside>
-    <?php
-    if(isset($_POST['submit'])) {
-        echo "Thanks for logging in";
-        header("Location:login.php?page=$name");
-    }
-    ?>
 
     <a href="index.php"><h1><span class="glyphicon glyphicon-ok"></span> The Advice Shop</h1></a>
 
