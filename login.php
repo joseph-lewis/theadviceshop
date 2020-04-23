27afcc7873ff106854fad8fcddb461b5b818f8cd
Joe <jlewi142@gmail.com>
2020-04-23 10:44:25 +1000

    test

--- login.php
+++ login.php
@@ -2,6 +2,6 @@
 error_reporting(E_ALL);
 session_start();
 $_SESSION['username'] = $_REQUEST['username'];
-//echo $_GET['page'];
+echo $_GET['page'];
 header("Location: ".$_GET['page'].".php");
 exit();
