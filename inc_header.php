2ebbb8c33151205f407fd80b414d70e9eb8629a0
Joe <jlewi142@gmail.com>
2020-04-23 11:08:44 +1000

    while loop implementation

--- inc_header.php
+++ inc_header.php
@@ -2,15 +2,22 @@
 $filename = substr(strrchr($_SERVER['SCRIPT_NAME'], "/"), 1); // missing '' around index - notice error
 $name = substr($filename, 0, strrpos($filename, ".")); ?>
 <header id="pageHeader">
-    <form method="post" action="login.php?page=<?php echo $name; ?>">
+    <form method="post" action="">
         <input type="text" name="username" id="username" placeholder="Username" value="<?= isset($_POST['something']) ? htmlspecialchars($_POST['something']) : '' ?>" />
         <input type="submit" name="submit" value="Login"/>
     </form></aside>
     <?php
     if(isset($_POST['submit'])) {
-        echo 'You entered: ', htmlspecialchars($_POST['username']);
+        $count = 0;
+        $dots = ".";
+        while ($count <= 4){
+            echo "" .$dots;
+            $dots .= ".";
+            ++$count;
+        }
     }
     ?>
+
     <a href="index.php"><h1><span class="glyphicon glyphicon-ok"></span> The Advice Shop</h1></a>
 
 
