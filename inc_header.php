42541955cb1413692b1fe49bf6f39c0f78ed9a26
Joe <jlewi142@gmail.com>
2020-04-23 11:00:18 +1000

    Testing again

--- inc_header.php
+++ inc_header.php
@@ -2,15 +2,13 @@
 $filename = substr(strrchr($_SERVER['SCRIPT_NAME'], "/"), 1); // missing '' around index - notice error
 $name = substr($filename, 0, strrpos($filename, ".")); ?>
 <header id="pageHeader">
-  <aside id="login"><form action="login.php?page=<?php echo $name; ?>" method="post"><input name="username" id="username" type="text" placeholder="Username"><input name="submit" type="submit" value="Login">q</form></aside>
-
-    <form method="post" action="">
-        <input type="text" name="something" value="<?= isset($_POST['something']) ? htmlspecialchars($_POST['something']) : '' ?>" />
-        <input type="submit" name="submit" />
-    </form>
+    <form method="post" action="login.php?page=<?php echo $name; ?>">
+        <input type="text" name="username" id="username" placeholder="Username" value="<?= isset($_POST['something']) ? htmlspecialchars($_POST['something']) : '' ?>" />
+        <input type="submit" name="submit" value="Login"/>
+    </form></aside>
     <?php
     if(isset($_POST['submit'])) {
-        echo 'You entered: ', htmlspecialchars($_POST['something']);
+        echo 'You entered: ', htmlspecialchars($_POST['username']);
     }
     ?>
     <a href="index.php"><h1><span class="glyphicon glyphicon-ok"></span> The Advice Shop</h1></a>
